dd2c5fb02e8b9fedb3c610f17ba7af26aff4dffe
Jack Guo <jackguo380@gmail.com>
2019-06-22 21:48:56 -0700

    Add install scripts

---
+++ alacritty-install-ubuntu-18.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+set -e
+
+if ! command -v rustup; then
+    echo "Install Rust to continue"
+    exit 1
+fi
+
+cd
+
+if [ ! -d alacritty/.git ]; then
+    rm -r -f alacritty
+    git clone https://github.com/jwilm/alacritty alacritty
+fi
+
+cd alacritty
+
+git pull
+
+git checkout cd8d537bed98559ed49c1465db84ecec5393119f
+
+rustup update stable
+
+sudo apt update
+
+sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
+
+cargo build --release
+
+cargo deb
---
+++ i3-install-ubuntu-18.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+set -e
+
+sudo echo
+
+if ! dpkg -l sur5r-keyring; then
+    cd /tmp
+
+    /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2019.02.01_all.deb keyring.deb SHA256:176af52de1a976f103f9809920d80d02411ac5e763f695327de9fa6aff23f416
+    sudo dpkg -i ./keyring.deb
+
+    rm keyring.deb
+fi
+
+if [ ! -f /etc/apt/sources.list.d/sur5r-i3.list ]; then
+    echo "deb http://debian.sur5r.net/i3/ bionic universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
+fi
+
+sudo apt update
+
+sudo apt install i3 i3lock-fancy i3blocks
