2a7f88050c93dfcb77c0b9f4e6c693246a690f53
Jack Guo <jackguo380@gmail.com>
2019-12-09 23:10:05 -0800

    Move desktop to i3status

---
+++ i3/soundoutput_yad.sh
@@ -0,0 +1,1 @@
+../utils/soundoutput_yad.sh
--- i3configure.sh
+++ i3configure.sh
@@ -7,9 +7,12 @@ repo_dir=$(realpath "$(dirname "$0")")
 i3baseconfig=$repo_dir/i3/baseconfig
 i3config=$repo_dir/i3/config
 
+i3statusbaseconfig=$repo_dir/i3status/baseconfig
+i3statusconfig=$repo_dir/i3status/config
+
 config_regex='^[ #]*\(.*\)#[ ]*--\([^#]\+\)--[ ]*$' 
 mapfile -t configs < <( \
-    grep "$config_regex" "$i3baseconfig" |
+    grep -h "$config_regex" "$i3baseconfig" "$i3statusbaseconfig" |
     sed -e "s/$config_regex/\2/" |
     sort -u)
 
@@ -38,16 +41,19 @@ for configuration in "${configurations[@]}"; do
 done
 
 
-echo "Copying $baseconfig to $i3config"
+echo "Copying $i3baseconfig to $i3config"
 cp "$i3baseconfig" "$i3config"
 
-echo "Commenting Out Unwanted Lines in $i3config"
-sed -i -e "s/$config_regex/#\1 # --\2--/" "$i3config"
+echo "Copying $i3statusbaseconfig to $i3statusconfig"
+cp "$i3statusbaseconfig" "$i3statusconfig"
+
+echo "Commenting Out Unwanted Lines in $i3config $i3statusconfig"
+sed -i -e "s/$config_regex/#\1 # --\2--/" "$i3config" "$i3statusconfig"
 
 for configuration in "${configurations[@]}"; do
-    echo "Enabling Wanted Lines for $configuration in $i3config"
+    echo "Enabling Wanted Lines for $configuration in $i3config $i3statusconfig"
     curconfig_regex="^[ #]*\(.*\)#[ ]*--$configuration--[ ]*$"
-    sed -i -e "s/$curconfig_regex/\1/" "$i3config"
+    sed -i -e "s/$curconfig_regex/\1/" "$i3config" "$i3statusconfig"
 done
 
 echo DONE
